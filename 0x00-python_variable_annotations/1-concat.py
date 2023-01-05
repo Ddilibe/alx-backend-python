995ddb588c8720dc5c18abf28d83f50aef813929
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-05 22:19:52 +0100

    [Chore]: Done with basic annotations concat task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Script for the basic annotation concat task """
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Function that concatenates two strings
+
+        Args:
+            :param @str1 - the first string
+            :param @str2 - the first string
+
+        Return:
+            Returns a string
+    """
+    return str1+str2
