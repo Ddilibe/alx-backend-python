44e6799a24b69c11a963cf8d6bef8a42f58c6777
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-05 22:25:01 +0100

    [Chore]: Done with basic annotation floor task.

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+""" Script for the basoc annotations floor task """
+
+
+def floor(n: float) -> int:
+    """
+        Function to obtain the floor of a float
+
+        Args:
+            :param @n - A float argument
+
+        Return:
+            Returns an integer
+    """
+    return int(n)
