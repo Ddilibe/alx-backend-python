f72cd06f03ecd77eb14d70a5b408c65b99c755de
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-05 22:14:00 +0100

    [Chore]: Done with the add task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" Script for the basic annotation task """
+
+
+def add(a: float, b: float) -> float:
+    """
+        Function that adds two variables together
+
+        Args:
+            :param @a - float varable
+            :param @b - float varable
+
+        Return:
+            returns a float
+    """
+    return (a+b)
