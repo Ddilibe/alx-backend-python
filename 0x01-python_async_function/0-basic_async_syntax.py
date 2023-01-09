96d86248a368b1e314a720204074abc821cc607d
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-09 14:18:19 +0100

    [Chore]: Wrote the file in accordance to pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
-
 """ Script testing the basics of asyncio """
+
+
 import random
 import asyncio
 
 
-async def wait_random(j: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """
         Function that takes an integer gets random
         waits asynchoronusly
 
         Args:
             :param @j [int] - First argument in the function
+            and the max delay to return.
 
         Return:
             This function returns an integer
     """
-    num = random.uniform(0, j)
+    num = random.uniform(0, max_delay)
     await asyncio.sleep(num)
     return num
