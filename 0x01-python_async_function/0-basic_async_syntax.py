5bad74eda8a6c4580c4e54d93001206f9a17ccd0
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-09 14:09:50 +0100

    [Feat]: Started learning Asynchronous IO programming

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" Script testing the basics of asyncio """
+import random, asyncio
+
+
+async def wait_random(j: int = 10) -> int:
+	"""
+		Function that takes an integer gets random
+		waits asynchoronusly
+
+		Args:
+			:param @j [int] - First argument in the function
+
+		Return:
+			This function returns an integer
+	"""
+	num = random.uniform(0, j)
+	await asyncio.sleep(num)
+	return num
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,10 @@
+# Python Asynchronous Function
+
+## Introduction
+
+## Learning Objective
+
+## Resources
+1. [ Async IO in Python: A Complete Walk Through](https://realpython.com/async-io-python/)
+2. [Asyncio - Asynchronous IO]()
+3. [Random.uniform]
