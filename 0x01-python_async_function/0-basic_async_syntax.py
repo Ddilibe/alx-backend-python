af9af620bc95e62c0bd9d83b6a6c50464866226a
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-09 14:15:39 +0100

    [Chore]: Wrote the file in accordance to pycodestyle

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,20 +1,21 @@
 #!/usr/bin/env python3
 
 """ Script testing the basics of asyncio """
-import random, asyncio
+import random
+import asyncio
 
 
 async def wait_random(j: int = 10) -> int:
-	"""
-		Function that takes an integer gets random
-		waits asynchoronusly
+    """
+        Function that takes an integer gets random
+        waits asynchoronusly
 
-		Args:
-			:param @j [int] - First argument in the function
+        Args:
+            :param @j [int] - First argument in the function
 
-		Return:
-			This function returns an integer
-	"""
-	num = random.uniform(0, j)
-	await asyncio.sleep(num)
-	return num
+        Return:
+            This function returns an integer
+    """
+    num = random.uniform(0, j)
+    await asyncio.sleep(num)
+    return num
