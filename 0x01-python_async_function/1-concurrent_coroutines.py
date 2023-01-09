ec06d0b00f4e7efc8d2f2db80b24c2123ba83ca7
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-09 17:27:40 +0100

    [Chore]: Done with some of the async python tasks

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Script that executes multiple coroutines at the same time with async """
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+        Function that calculates the time taken to finidh a particular coroutine
+
+        Args:
+            :param @n - the first argument and the number of times to loop over wait_random
+            :param @max_delay - The second argument and the time to delay the function
+
+        Return:
+            Function returns a list of floats
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
