3938ec143ea317dd3cec1064890c4fe31f93a663
Ddilibe <franklinfidelugwuowo@gmail.com>
2023-01-28 16:13:17 +0100

    [Chore]: Done with task 3
    Mock HTTP calls

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Script for parameterizing a unit test """
-from utils import access_nested_map
-from unittest import TestCase
+from utils import access_nested_map, get_json
 from parameterized import parameterized
+from unittest.mock import patch
+from unittest import TestCase
+import requests
 
 
 class TestAccessNestedMap(TestCase):
@@ -25,10 +27,24 @@ class TestAccessNestedMap(TestCase):
 		({}, ("a",)),
 		({"a": 1}, ("a", "b"))
 	])
-	def test_Access_nested_map_exception(self, nested_map, path):
+	def test_access_nested_map_exception(self, nested_map, path):
 		"""
 			Method for unittesting access nested map
 		"""
 		args = [nested_map, path]
 		with self.assertRaises(KeyError):
 			access_nested_map(*args)
+
+class TestGetJson(TestCase):
+	"""docstring for TestGetJson"""
+	@parameterized.expand([
+		("http://example.com", {"payload": True}),
+		("http://holberton.io", {"payload": False})
+	])
+	@patch('requests.get')
+	def test_get_json(self, test_url, test_payload, patch_method):
+		""" Method for testing the get_jsin function in utils """
+		patch_method.return_value.json.return_value = test_payload
+		a = get_json(test_url)
+		self.assertEqual(a, test_payload)
+		patch_method.assert_called_once_with(test_url)
